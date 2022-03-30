ee8ad95a5723347d632e4f6eed08a20e5cf3359b
eugeniabalan <101556669+eugeniabalan@users.noreply.github.com>
2022-03-30 19:54:51 +0200

    HTML CSS validation
    HTML CSS validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-function menuToggle () {
+function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className == 'navtoggle') {
+  if (x.className === 'navtoggle') {
     x.className += ' responsive';
   } else {
   x.className = 'navtoggle';
