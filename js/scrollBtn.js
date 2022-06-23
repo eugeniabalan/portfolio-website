52e6766daea44d392bd55765fbfc1842d5a082f1
nickbalan <nickolae.balan@gmail.com>
2022-06-23 22:28:53 +0200

    Refactoring the Scroll to Top Button

--- js/scrollBtn.js
+++ js/scrollBtn.js
@@ -1,5 +1,5 @@
 //Get the button
-var mybutton = document.getElementById("scroolBtn");
+var mybutton = document.getElementById("scrollBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
